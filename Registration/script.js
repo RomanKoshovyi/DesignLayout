f98576d756556a02b9c70804fef062e18b3fefc6
Roman <romanrokel@gmail.com>
2021-11-13 01:45:23 +0200

    Site was filled in Home and News pages. Footer upgraded. Added a registration form. New Media subpage - Merch (with popup windows). Early access!

---
+++ Registration/script.js
@@ -0,0 +1,14 @@
+﻿function func(evt){
+    evt.preventDefault();
+    let pass1 = document.getElementById("pass1");
+    let pass2 = document.getElementById("pass2");
+    if (pass1.value != pass2.value){
+        alert("Passwords dont match! Please, try again!");
+        pass1.value = '';
+        pass2.value = '';
+    }
+    else{
+        alert("Your registration completed successfully! You are going to be returned to Skillet Band page right now!");
+        window.location.href = "https://romankoshovyi.github.io/DesignLayout/";
+    }
+}
--- script.js
+++ script.js
@@ -2,21 +2,32 @@ let
     gallery = document.querySelector(".gallery"),
     home = document.querySelector(".home"),
     news = document.querySelector(".news");
+    merch = document.querySelector(".merch");
+
+function MerchShow(){
+    home.style.display = 'none';
+    news.style.display = 'none';
+    gallery.style.display = 'none';
+    merch.style.display = 'flex';
+}
 
 function GalleryShow(){
     home.style.display = 'none';
     news.style.display = 'none';
+    merch.style.display = 'none';
     gallery.style.display = 'flex';
 }
 
 function HomeShow(){
     news.style.display = 'none';
     gallery.style.display = 'none';
+    merch.style.display = 'none';
     home.style.display = 'flex';
 }
 
 function NewsShow(){
     home.style.display = 'none';
     gallery.style.display = 'none';
+    merch.style.display = 'none';
     news.style.display = 'flex';
 }
